17ecbfd5af60d992201be7fed25d8a2ac7dd4e36
Pacharapol Withayasakpunt <polv@polv.cc>
2021-10-04 23:00:09 +0700

    move files to public

--- api/index.py
+++ api/index.py
@@ -2,28 +2,20 @@ import re
 from pathlib import Path
 
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
 
 from gtts import gTTS
 
 app = FastAPI()
-app.mount("/f", StaticFiles(directory="static"))
-
-
-@app.get("/")
-def docs():
-    return RedirectResponse("/docs")
-
 
 @app.get("/api/tts")
 def api_tts(q: str, lang: str):
     safe_q = re.sub(r"\W+", q, " ")
     print(Path.cwd())
-    save_path = Path("static", lang, safe_q + ".mp3")
+    save_path = Path("public", lang, safe_q + ".mp3")
 
     if not save_path.exists():
-        Path("static", lang).mkdir(exist_ok=True)
+        Path("public", lang).mkdir(exist_ok=True)
         gTTS(q, lang=lang).save(str(save_path))
 
     def iter_file():
